a3ab7b08581a031f018a3a09dc7d43cb8f4fdd24
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2022-10-24 17:55:15 +0900

    Initial commit

---
+++ app/src/main/java/com/kkalkkalparrot/daily/MainActivity.java
@@ -0,0 +1,14 @@
+package com.kkalkkalparrot.daily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
