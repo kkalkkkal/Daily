e66b91be915b59618d1578e6cad417f95257f563
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2022-11-26 19:47:17 +0900

    journal_list adapter

--- app/src/main/java/com/kkalkkalparrot/daily/JournalAdapter.java
+++ app/src/main/java/com/kkalkkalparrot/daily/JournalAdapter.java
@@ -1,7 +1,11 @@
 package com.kkalkkalparrot.daily;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,31 +14,48 @@ import java.util.ArrayList;
 
 public class JournalAdapter extends RecyclerView.Adapter<JournalAdapter.JournalViewHolder> {
 
+    private ArrayList<JournalInfo> mList;
 
-    public JournalAdapter(ArrayList<JournalInfo> mArrayList) {
+    public class RecipeViewHolder extends RecyclerView.ViewHolder{
+        protected TextView recipeName;
+        protected String documentName;
+
+        public RecipeViewHolder(View view){
+            super(view);
+            this.recipeName = (TextView)view.findViewById(R.id.JournalName);
+            view.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), LookJournal.class);
+                    Log.e("RecipeAdpater", documentName);
+                    intent.putExtra("recipeName", documentName);
+                    v.getContext().startActivity(intent);
+                }
+            });
+        }
+    }
+    public JournalAdapter(ArrayList<JournalInfo> list){
+        this.mList=list;
     }
 
     @NonNull
     @Override
-    public JournalAdapter.JournalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recipe_info, parent, false);
+        RecipeViewHolder viewHolder = new RecipeViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull JournalAdapter.JournalViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+        Log.e("RecipeAdapter", mList.get(position).getRecipeName());
+        holder.recipeName.setText(mList.get(position).getRecipeName());
+        holder.documentName = mList.get(position).getDocumentName();
     }
-
 
     @Override
     public int getItemCount() {
-        return 0;
-    }
-
-
-    public class JournalViewHolder extends RecyclerView.ViewHolder{
-        public JournalViewHolder(@NonNull View itemView) {
-            super(itemView);
-        }
+        return (null != mList?mList.size() : 0);
     }
 }
--- app/src/main/java/com/kkalkkalparrot/daily/journalList.java
+++ app/src/main/java/com/kkalkkalparrot/daily/journalList.java
@@ -23,51 +23,10 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 
 // 선택한 날짜의 저널 리스트 보기
-public class journalList extends Fragment {
+public class journalList extends AppCompatActivity {
 
-    private ArrayList<JournalInfo> mArrayList;
-    private JournalAdapter mAdapter;
-    private RecyclerView mRecyclerView;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.journal_list, container, false);
-    }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        mRecyclerView = (RecyclerView)view.findViewById(R.id.JournalRecyclerView);
-        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(view.getContext());
-        mRecyclerView.setLayoutManager(mLinearLayoutManager);
-
-        mArrayList = new ArrayList<>();
-
-        //여기에 mArrayList에 데이터 넣는거 필요함
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("Journal").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()){
-                    int count = 0;
-                    for (DocumentSnapshot document : task.getResult()){
-                        String JournalName = (String)document.get("TITLE");
-                        JournalInfo inputJournal = new JournalInfo(JournalName, document.getId());
-                        mArrayList.add(inputJournal);
-                        Log.e("JournalFragment", JournalName + " input!");
-                    }
-                    mAdapter = new JournalAdapter(mArrayList);
-                    setmAdapter();
-                }
-            }
-        });
-    }
-
-    void setmAdapter(){
-        mRecyclerView.setAdapter(mAdapter);
-    }
 
 
 }
