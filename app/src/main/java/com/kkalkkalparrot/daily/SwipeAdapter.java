e80778f2e46ee45b7a3f0533ba0ba06b1781af63
lkmisem <lkmisem@konkuk.ac.kr>
2022-11-27 11:34:32 +0900

    Finder DB연결 및 친구추가기능 추가

--- app/src/main/java/com/kkalkkalparrot/daily/SwipeAdapter.java
+++ app/src/main/java/com/kkalkkalparrot/daily/SwipeAdapter.java
@@ -1,36 +1,47 @@
 package com.kkalkkalparrot.daily;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
+import java.util.Map;
 
 public class SwipeAdapter extends BaseAdapter {
 
     private Context context;
-    private List<Integer> list;
+    private List<Map<String, Object>> list;
 
-    public SwipeAdapter(Context context, List<Integer> list) {
+    public SwipeAdapter(Context context, List<Map<String, Object>> list) {
         this.context = context;
         this.list = list;
     }
 
     @Override
     public int getCount() {
-        return 20;
+        return list.size();
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public Map<String, Object> getItem(int i) {
+        return list.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
@@ -38,9 +49,18 @@ public class SwipeAdapter extends BaseAdapter {
         View view;
         if (convertView == null){
             view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_koloda, parent, false);
+            Map<String,Object> tags = (Map<String,Object>)getItem(i).get("level");
+            String tagStr = "관심분야: | ";
+            for ( String key : tags.keySet()){
+                tagStr+= key + " LV" + tags.get(key)+" | ";
+            }
+            ((TextView)view.findViewById(R.id.finder_userName)).setText((String)getItem(i).get("nickName"));
+            ((TextView)view.findViewById(R.id.finder_userLevel)).setText(tagStr);
+            Glide.with(parent).load(getItem(i).get("image")).into((ImageView)view.findViewById(R.id.finder_user_image));
         } else {
             view = convertView;
         }
         return view;
     }
+
 }
